bc6cbe8cb245cea81a3f3d9225fefc0dcfcaec5e
Brian Pham <br.pham13@gmail.com>
2024-09-12 13:30:25 -0500

    add useful navigation keymaps

--- init.lua
+++ init.lua
@@ -191,6 +191,12 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Set the cursor view to the center of the viewport
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 

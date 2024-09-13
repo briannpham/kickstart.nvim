9ed043caf2f78214713920c5d96bc9f2fd655672
Brian Pham <br.pham13@gmail.com>
2024-09-12 20:28:44 -0500

    add copilot and git_commits keymap

--- init.lua
+++ init.lua
@@ -237,6 +237,8 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-surround',
+  'github/copilot.vim',
   {
     'numToStr/Comment.nvim',
     opts = {
@@ -508,6 +510,7 @@ require('lazy').setup({
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+      vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
